913dfe086e4f012ed6e7948f83110dcd6c71c0bb
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-28 19:33:41 +0300

    Initial Commit

---
+++ ConnectAudioUnitsTogether/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ConnectAudioUnitsTogether
+//
+//  Created by Panayotis Matsinopoulos on 28/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
